62c3792ffde2437e799ba2cc890150d679901ebf
Erika Anderson <erikaanderson@Carmens-MacBook-Air.local>
2019-06-17 17:51:42 -0400

    Portfolio Website Redesign 2019
    - added new fonts to use 'Lora' and 'Lato'
    - updated colours
    - redo layout of homepage
    - renamed 'work' file to 'chango-dashboard' to avoid confusion
    - add in new works and pages based on categories: product, web, and graphic
    - tweak mobile styles for devices
    - add in some fade in animations
    - add new copy for project descriptions
    - add img alt descriptions

--- helper.js
+++ helper.js
@@ -28,4 +28,42 @@ $(document).ready(function() {
         $(this).parent().addClass('active');
         $(this).parent().siblings().removeClass('active');
     });
+    $('#mock-area .img-toggle a').click(function(event) {
+            event.preventDefault();
+    });
+    $('#mock-area .link-1').click(function() {
+        $(this).closest('.project-area').find($('.desktop')).show();
+        $(this).closest('.project-area').find($('.mobile')).hide();
+
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+    });
+    $('#mock-area .link-2').click(function() {
+        $(this).closest('.project-area').find($('.mobile')).addClass('display-flex');
+        $(this).closest('.project-area').find($('.desktop')).hide();
+
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+    });
+    // disable dropdown function on portfolio link when mobile menu active to avoid double menu bug
+    if($('.navbar-toggle').css('display') == 'block') {
+        $('.navbar-nav .dropdown .dropdown-toggle').attr('data-toggle', false);
+        $('.navbar-nav .contact-link').css('clear', 'both');
+    } else {
+        $('.navbar-nav .dropdown .dropdown-toggle').attr('data-toggle', 'dropdown');
+        $('.navbar-nav .contact-link').css('float', 'left');
+    }
+});
+// add shadow to navigation on scroll
+$(window).scroll(function() {
+var st = $(window).scrollTop()
+var nav = $('#site-navigation')
+
+    if ($('.nav-transition').length > 0) {
+        if (st > $('.nav-transition').offset().top - nav.height()) {
+            nav.addClass('shadow');
+        } else {
+            nav.removeClass('shadow');
+        }
+    }
 });
